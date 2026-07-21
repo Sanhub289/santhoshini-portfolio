338017656d1300a7a80edc198dc749dd77dd4541
Sanhub289 <vallakatlasanthoshini@gmail.com>
2026-07-21 15:34:01 -0400

    fix base path for netlify

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // IMPORTANT: Change 'santhoshini-portfolio' to your actual GitHub repo name
-  base: '/santhoshini-portfolio/',
+  base: '/',
   build: {
     outDir: 'dist',
   },
